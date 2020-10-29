a02acabd50b6ce89fe8df5ccdaad4cb12dab6dd7
JamesPerlman <jam.e.perl@gmail.com>
2020-10-29 09:31:47 -0700

    The test

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export * from './add-item-button';
 export * from './container';
 export * from './item-list';
-export * from './option-view';
 export * from './selection-view';
 export * from './submit-button';
--- src/components/selection-view.js
+++ src/components/selection-view.js
@@ -1,44 +1,13 @@
-import { FlatList, StyleSheet } from 'react-native';
 import React, { Component } from 'react';
 
-import { OptionView } from './option-view';
+import { StyleSheet } from 'react-native';
 
 export class SelectionView extends Component {
 
   render() {
-    const { data, initialIndex, onSelect, selectedIndex, style, ...props } = this.props;
-
-    return (
-      <FlatList
-        initialScrollIndex={initialIndex}
-        data={data}
-        style={[styles.flatList, style]}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(option, index) => `option_${index}`}
-        renderItem={({ item, index }) => (
-          <OptionView
-            option={item}
-            onPress={onSelect && (() => onSelect(index))}
-            style={styles.option}
-            selected={index === selectedIndex}
-          />
-        )}
-      />
-    );
+    return null;
   }
 }
 
 const styles = StyleSheet.create({
-  flatList: {
-    width: '100%',
-    height: 88,
-  },
-  option: {
-    marginTop: 8,
-    height: 88,
-    width: 100,
-    marginRight: 4,
-    marginLeft: 4,
-  }
 });
